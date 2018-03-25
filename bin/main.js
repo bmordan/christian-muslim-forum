656a5e97080d4a35f937af9808f7bfee88c92703
bmordan <bernardmordan@gmail.com>
2018-03-25 10:19:25 +0100

    updates events page with date

--- bin/main.js
+++ bin/main.js
@@ -2,15 +2,6 @@ const build = require('./build')
 const tasks = require('./tasks')
 const buildArticles = require('./articles')
 
-// const cb = () => console.log('done')
-
-// build(tasks[0], cb) // Home
-// build(tasks[1], cb) // About
-// build(tasks[2], cb) // Contact
-// build(tasks[3], cb) // People
-// build(tasks[4], cb) // Events
-// build(tasks[5], cb) // Article
-
 function buildTasks (tasks) {
   if (!tasks.length) return buildArticles(true, null)
 
@@ -21,3 +12,12 @@ function buildTasks (tasks) {
 }
 
 buildTasks(tasks)
+
+// const cb = () => console.log('done')
+
+// build(tasks[0], cb) // Home
+// build(tasks[1], cb) // About
+// build(tasks[2], cb) // Contact
+// build(tasks[3], cb) // People
+// build(tasks[4], cb) // Events
+// build(tasks[5], cb) // Article
